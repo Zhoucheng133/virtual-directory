47db11a349c48149e20bf3be4b120befe2dbb3fd
zhoucheng <zhoucheng133@outlook.com>
2023-11-15 10:03:27 +0800

    添加剪贴板

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/antd.css'
+import VueClipboard from 'vue-clipboard2';
+
+Vue.use(Antd);
+Vue.use(VueClipboard);
 
 Vue.use(Antd)
 

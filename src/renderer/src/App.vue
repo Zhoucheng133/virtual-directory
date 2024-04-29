6295c98a85b28f9f30ef2b137cce19d06c9a9c98
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-04-29 17:54:15 +0800

    初始化

--- src/renderer/src/App.vue
+++ src/renderer/src/App.vue
@@ -1,26 +1,15 @@
-<script setup lang="ts">
-import Versions from './components/Versions.vue'
-
+<!-- <script setup lang="ts">
 const ipcHandle = () => window.electron.ipcRenderer.send('ping')
-</script>
+</script> -->
 
 <template>
-  <img alt="logo" class="logo" src="./assets/electron.svg" />
-  <div class="creator">Powered by electron-vite</div>
-  <div class="text">
-    Build an Electron app with
-    <span class="vue">Vue</span>
-    and
-    <span class="ts">TypeScript</span>
-  </div>
-  <p class="tip">Please try pressing <code>F12</code> to open the devTool</p>
-  <div class="actions">
-    <div class="action">
-      <a href="https://electron-vite.org/" target="_blank" rel="noreferrer">Documentation</a>
-    </div>
-    <div class="action">
-      <a target="_blank" rel="noreferrer" @click="ipcHandle">Send IPC</a>
-    </div>
-  </div>
-  <Versions />
+  Hello!
+  <button @click="close">关闭</button>
 </template>
+
+
+<script setup lang="ts">
+const close=()=>{
+  window.electron.ipcRenderer.send('close');
+}
+</script>

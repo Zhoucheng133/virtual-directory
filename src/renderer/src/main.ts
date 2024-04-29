384f1d87e1866a5a97bbbe28652e63fe8413ba4d
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-04-29 19:56:17 +0800

    导入bs-icons

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -2,5 +2,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 const pinia = createPinia()
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 createApp(App).use(pinia).mount('#app')

320318aad6c949b5591358c1b19370f45da004e4
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-04-29 19:59:27 +0800

    导入antd

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import { createPinia } from 'pinia'
-const pinia = createPinia()
-import 'bootstrap-icons/font/bootstrap-icons.css'
+import { createApp } from 'vue';
+import Antd from 'ant-design-vue';
+import 'ant-design-vue/dist/reset.css';
+import App from './App.vue';
+import { createPinia } from 'pinia';
+const pinia = createPinia();
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
-createApp(App).use(pinia).mount('#app')
+createApp(App).use(Antd).use(pinia).mount('#app');

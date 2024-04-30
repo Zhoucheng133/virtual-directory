7dd412c673d2f97d511c8afa72246204210bed07
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-04-30 17:02:03 +0800

    添加右侧的页面

--- src/renderer/src/components/RightSide.vue
+++ src/renderer/src/components/RightSide.vue
@@ -1,11 +1,30 @@
 <template>
   <div class="bg">
-    
+    <div class="logPanel">
+
+    </div>
+    <a-button class="runButton" type="primary">启动服务</a-button>
   </div>
 </template>
 
 <style scoped>
+.runButton{
+  margin-top: 20px;
+
+}
+.logPanel{
+  width: 100%;
+  flex: 1;
+  background-color: rgb(250, 250, 250);
+  border-radius: 10px;
+}
 .bg{
   width: 100%;
+  user-select: none;
+  display: flex;
+  flex-direction: column;
+  padding: 30px;
+  padding-top: 20px;
+  padding-bottom: 40px;
 }
 </style>

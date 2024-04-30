ba3a96761a363f9ac472416f30d7cdafa68fb8d0
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-04-30 16:41:15 +0800

    完成表单设计

---
+++ src/renderer/src/components/LeftSide.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="bg">
+    <a-form :label-col="{ style: { width: '70px' } }">
+      <a-form-item label="简易模式">
+        <div style="display: flex; align-items: center;">
+          <a-switch v-model:checked="formData().easyMode" style="margin-right: 20px;"></a-switch>
+          <a-tooltip placement="bottom" title="简易模式下只能从客户端上传文件到本机">
+            <i class="bi bi-question"></i>
+          </a-tooltip>
+        </div>
+      </a-form-item>
+      <a-form-item label="端口设置">
+        <a-input-number v-model:value="formData().port" :min="1000" :max="9999" style="margin-right: 20px;"></a-input-number> 
+      </a-form-item>
+      <a-form-item label="映射目录">
+        <div style="display: flex; align-items: center;">
+          <a-input v-model:value="formData().dir"></a-input>
+          <a-button style="margin-left: 10px;">浏览</a-button>
+        </div>
+      </a-form-item>
+      <a-form-item label="设置权限">
+        <div style="display: flex; flex-direction: column; margin-top: 5px;">
+          <a-checkbox v-model:checked="formData().read" :disabled="formData().easyMode">读取</a-checkbox>
+          <a-checkbox v-model:checked="formData().write" :disabled="formData().easyMode">写入</a-checkbox>
+          <a-checkbox v-model:checked="formData().del" :disabled="formData().easyMode">删除</a-checkbox>
+        </div>
+      </a-form-item>
+      <a-form-item label="需要登陆">
+        <a-switch v-model:checked="formData().useLogin"></a-switch>
+      </a-form-item>
+      <a-form-item label="用户名">
+        <a-input v-model:value="formData().username" :disabled="!formData().useLogin"></a-input>
+      </a-form-item>
+      <a-form-item label="密码">
+        <a-input-password v-model:value="formData().password" :disabled="!formData().useLogin"></a-input-password>
+      </a-form-item>
+    </a-form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import formData from '@renderer/stores/formData';
+</script>
+
+<style scoped>
+.bg{
+  width: 100%;
+  padding: 30px;
+  padding-top: 10px;
+  user-select: none;
+}
+.bi-question{
+  font-size: 20px;
+}
+</style>
---
+++ src/renderer/src/stores/formData.ts
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+import { ref, watch } from "vue";
+
+export default defineStore('formData', ()=>{
+  let easyMode=ref(false);
+  let port=ref(8081);
+  let dir=ref("");
+  let write=ref(true);
+  let read=ref(true);
+  let del=ref(true);
+  let useLogin=ref(false);
+  let username=ref("");
+  let password=ref("");
+
+  watch(read, (newVal)=>{
+    if(!newVal){
+      del.value=false;
+      easyMode.value=true;
+    }
+  })
+  watch(easyMode, (newVal)=>{
+    if(newVal){
+      read.value=false;
+      write.value=true;
+      del.value=false;
+    }
+  })
+
+  return { easyMode, port, dir, write, read, del, useLogin, username, password };
+})

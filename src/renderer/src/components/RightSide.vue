ae6695f50dc841a0629799d6072865272429880d
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-04-30 21:20:28 +0800

    完成日志显示

--- src/renderer/src/components/RightSide.vue
+++ src/renderer/src/components/RightSide.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="bg">
     <div class="logPanel">
-
+      <div class="logItem" v-for="(item, index) in logData().log" :key="index">
+        <div class="logContent">{{ item.content }}</div>
+        <div class="logTime">{{ item.time }}</div>
+      </div>
     </div>
     <a-button class="runButton" type="primary">启动服务</a-button>
   </div>
@@ -17,15 +20,24 @@ onMounted(()=>{
 </script>
 
 <style scoped>
+.logTime{
+  margin-left: auto;
+}
+.logItem{
+  display: grid;
+  grid-template-columns: auto 70px;
+  font-size: 14px;
+}
 .runButton{
   margin-top: 20px;
-
 }
 .logPanel{
   width: 100%;
   flex: 1;
   background-color: rgb(250, 250, 250);
   border-radius: 10px;
+  overflow: auto;
+  padding: 20px;
 }
 .bg{
   width: 100%;

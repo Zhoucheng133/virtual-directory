97045b861d3a560c8653706ba3e7928dd95775fa
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-05-02 17:16:38 +0800

    添加基础数据类型

--- ui/src/stores/index.ts
+++ ui/src/stores/index.ts
@@ -2,7 +2,23 @@ import { defineStore } from "pinia";
 import { ref } from "vue";
 
 export default defineStore('index', ()=>{
-  let path=ref<string[]>(['根目录', '测试目录1', '测试目录2', '测试目录3', '测试目录4']);
 
-  return { path };
+  interface data{
+    id: string,
+    isFile: boolean,
+    isSelected: boolean,
+    fileName: string,
+    size: number,
+  }
+
+  let path=ref<string[]>(['根目录', '测试目录1', '测试目录2', '测试目录3', '测试目录4']);
+  let data=ref<data[]>([]);
+  
+  const getData=()=>{
+
+  }
+
+  getData();
+
+  return { path, data };
 })

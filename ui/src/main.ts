bb82139b2f738a6d20a0ea0ecbda697c0bc9123b
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-05-02 16:40:43 +0800

    添加bs-icon依赖

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import { createPinia } from 'pinia'
 import Antd from 'ant-design-vue';
 import 'ant-design-vue/dist/reset.css';
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 
 const pinia = createPinia()
 

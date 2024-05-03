30821a228134e228e0c32752c62f9c0089b8d958
DESKTOP-AUU8MEV\周诚 <zhoucheng133@outlook.com>
2024-05-03 18:06:17 +0800

    添加测试页

--- ui/src/components/Login.vue
+++ ui/src/components/Login.vue
@@ -1,5 +1,16 @@
 <template>
   <div>
-    
+    这是登陆页面: {{ num }}
+    <button @click="test">测试</button>
   </div>
 </template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+let num=ref(0);
+const test=()=>{
+  num.value+=1;
+}
+
+</script>

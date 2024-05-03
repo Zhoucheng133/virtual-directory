0607b2392ed8765149c1fa6731d5426653d12b14
周诚 <zhoucheng133@outlook.com>
2024-05-03 21:11:12 +0800

    添加登陆方法

--- ui/src/stores/index.ts
+++ ui/src/stores/index.ts
@@ -18,12 +18,33 @@ export default defineStore('index', ()=>{
   const baseURL="http://127.0.0.1:8088";
   let isLogin=ref(false);
 
+  const loginController=async (username: string, password: string)=>{
+    const loginfeedback=await axios.get(baseURL+'/api/login', {
+      params: {
+        username: username,
+        password: CryptoJS.SHA256(password).toString()
+      }
+    });
+    if(loginfeedback.data){
+      return true;
+    }
+    return false;
+  }
+
   const init=async ()=>{
     const needLogin=await axios.get(baseURL+'/api/needLogin');
     if(needLogin.data==false){
       isLogin.value=true;
+    }else{
+      const userData=localStorage.getItem('userData');
+      if(userData){
+        const jsonData=JSON.parse(userData);
+        if(await loginController(jsonData.username, jsonData.password)){
+          isLogin.value=true;
+        }
+      }
     }
   }
 
-  return { path, data, isLogin, init };
+  return { path, data, isLogin, init, loginController };
 })

b0fcc911f00739ba71bd2396b8e5fe7ba3af0232
周诚 <zhoucheng133@outlook.com>
2024-06-05 20:10:38 +0800

    合并页面

---
+++ src/renderer/src/components/PageContent.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="bg">
+    <div class="formView">
+      <div class="title">端口</div>
+      <a-input-number v-model:value="formData().port" :min="1000" :max="9999" style="margin-right: 20px;"  :disabled="mainData().onRunning"></a-input-number> 
+      <div class="title">映射目录</div>
+      <div style="display: flex; align-items: center;">
+        <a-tooltip placement="bottom" :title="formData().dir">
+          <a-input v-model:value="formData().dir" spellcheck="false" disabled></a-input>
+        </a-tooltip>
+        <a-button style="margin-left: 10px;" @click="formData().selectDir" :disabled="mainData().onRunning">浏览</a-button>
+      </div>
+      <div class="title">权限</div>
+      <div style="display: flex; margin-top: 5px;">
+        <a-checkbox v-model:checked="formData().write" :disabled="mainData().onRunning || formData().easyMode">写入</a-checkbox>
+        <a-checkbox v-model:checked="formData().del" :disabled="mainData().onRunning || formData().easyMode">删除</a-checkbox>
+      </div>
+      <div class="title">需要登录</div>
+      <div><a-switch v-model:checked="formData().useLogin" :disabled="mainData().onRunning"></a-switch></div>
+      <div class="title">用户名</div>
+      <a-input v-model:value="formData().username" :disabled="!formData().useLogin || mainData().onRunning"></a-input>
+      <div class="title">密码</div>
+      <a-input-password v-model:value="formData().password" :disabled="!formData().useLogin || mainData().onRunning"></a-input-password>
+    </div>
+    <div class="ipPanel">
+      <i class="bi bi-router"></i>
+      {{ ip }}:{{ formData().port }}
+      <div class="openLink" @click="openLink">打开链接</div>
+    </div>
+    <div class="buttonArea" style="display: flex; justify-content: space-between;">
+      <a-button @click="copyIP">复制地址</a-button>
+      <a-button class="runButton" type="primary" @click="mainData().toggleRun" :danger="mainData().onRunning ? true : false">
+        {{ mainData().onRunning ? '停止服务' : '启动服务' }}
+      </a-button>
+    </div>
+    <div class="info" @click="toGithub">
+      <div style="margin-right: 5px; color: lightgrey; font-size: 13px">Click Here to see</div>
+      <img src="../assets/icon.png" alt="" height="25px">
+      <div style="margin-left: 5px; margin-right: 5px; color: lightgrey; font-size: 13px">In</div>
+      <i class="bi bi-github"></i>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import formData from '@renderer/stores/formData';
+import mainData from '@renderer/stores/mainData';
+import { message } from 'ant-design-vue';
+import { onMounted, ref } from 'vue';
+const toGithub=()=>{
+  window.open("https://github.com/Zhoucheng133/virtual-directory")
+}
+onMounted(()=>{
+  const form=localStorage.getItem('form');
+  if(form){
+    const data=JSON.parse(form);
+    formData().setForm(data);
+  }
+})
+const copyIP=()=>{
+  navigator.clipboard.writeText(ip+':'+formData().port);
+  message.success("复制成功！");
+}
+let ip=ref('');
+const openLink=()=>{
+  if(mainData().onRunning){
+    window.open(`http://localhost:${formData().port}`)
+    console.log(`http:localhost:${formData().port}`);
+    
+  }else{
+    message.info("服务没有在运行")
+  }
+}
+
+onMounted(()=>{
+  window.electron.ipcRenderer.invoke('getIP').then((response)=>{
+    ip.value=response
+  })
+})
+</script>
+
+<style scoped>
+.info{
+  margin-top: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+}
+.formView{
+  display: grid;
+  grid-template-columns: 70px auto;
+  margin-bottom: 20px;
+  align-items: center;
+  grid-column-gap: 10px;
+  grid-row-gap: 20px;
+}
+.buttonArea{
+  display: flex;
+  align-items: center;
+  margin-top: 20px;
+}
+.openLink:hover{
+  color: #3075ca;
+}
+.openLink{
+  color: #4096ff;
+  cursor: pointer;
+  transition: color linear .2s;
+  margin-left: auto;
+}
+.bi-router{
+  margin-right: 10px;
+  font-size: 20px;
+}
+.ipPanel{
+  display: flex;
+  align-items: center
+}
+.bg{
+  width: 100%;
+  padding: 30px;
+  padding-top: 20px;
+  user-select: none;
+}
+.bi-question{
+  font-size: 20px;
+}
+</style>
